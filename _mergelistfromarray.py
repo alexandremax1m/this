f12a58fd7ef46310d85875aa6454a2b9324b31f3
alexandremax1m <119704433+alexandremax1m@users.noreply.github.com>
2022-12-07 21:34:37 +0100

    Add files via upload

---
+++ _mergelistfromarray.py
@@ -0,0 +1,40 @@
+import multiprocessing 
+
+from heapq import merge
+from datetime import datetime
+from multiprocessing import Process
+
+mylist1, mylist2, mylist3 = [],[],[]
+
+myarray = multiprocessing.Array('b',(0,1,2,3,4,5,6,7,8,9))
+
+def _mergelistsfromarray(n):
+	
+	now = datetime.now()
+	
+	for x in range(n):		
+		x = []
+		mylist1.append(tuple(x))
+		mylist2.append(tuple(x))
+	
+	for x in range(n):		
+		if not type(mylist1[x]) == list:
+			mylist1[x] = list(mylist1[x])
+		if not mylist1[x]:
+			mylist1[x] = tuple([x for x in myarray])
+	
+	for x in range(n):
+		if not mylist2[x]:
+			mylist2[x] = merge(mylist1)
+		mylist3.append(merge(mylist2))
+	
+	print(f"{datetime.now()-now}")
+	print(f"{(n**2)} integers stored in mylist1")
+	print(f"{(n**3)} integers stored in mylist2")
+	print(f"{(n**4)} integers stored in mylist3")
+
+if __name__ != "__main__":
+	
+	n = int(input("What Number To Test Sir :: "))
+	test1process = Process(target=_mergelistsfromarray(n))
+	test1process.start()
